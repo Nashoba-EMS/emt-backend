b013bfbac46cbe1492d8be7c0859233ef30d445f
Jeff Taylor-Chang <jeff@jefftc.com>
2021-01-03 12:50:19 -0500

    Added dependencies and updated config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module'
   },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   rules: {
     strict: 2,
-    "@typescript-eslint/camelcase": 1,
-  },
+    '@typescript-eslint/camelcase': 1,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/no-explicit-any': 0
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const slsw = require("serverless-webpack");
-const nodeExternals = require("webpack-node-externals");
-const path = require("path");
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
   entry: slsw.lib.entries,
-  target: "node",
+  target: 'node',
   // Generate sourcemaps for proper error messages
-  devtool: "source-map",
+  devtool: 'source-map',
   // Since 'aws-sdk' is not compatible with webpack,
   // we exclude all node dependencies
   externals: [nodeExternals()],
-  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   optimization: {
     // We no not want to minimize our code.
-    minimize: false,
+    minimize: false
   },
   performance: {
     // Turn off size warnings for entry points
-    hints: false,
+    hints: false
   },
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" },
-    ],
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
   },
   resolve: {
     alias: {
-      src: path.resolve(__dirname, "src/"),
-    },
-  },
+      src: path.resolve(__dirname, 'src/')
+    }
+  }
 };
