41424f7d38c374b79caaf8c8c7e76a43ef9e90fc
Jeff Taylor-Chang <jeff@jefftc.com>
2021-01-03 13:31:28 -0500

    Added logging to hello handler

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -3,6 +3,10 @@ import middyfy from '../middleware';
 import { HTTPRawHandler } from './handler';
 
 const _handler: HTTPRawHandler = async (event) => {
+  console.log('pathParameters', event.pathParameters);
+  console.log('queryStringParameters', event.queryStringParameters);
+  console.log('body', event.body);
+
   return {
     message: 'Hello World'
   };
