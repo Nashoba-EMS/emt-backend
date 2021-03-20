dc5922284ce3a9e642094b2c2cddc419ca84e307
Jeff Taylor-Chang <jeff@jefftc.com>
2021-03-20 18:26:47 -0400

    Added schedule availability type

--- src/models/schedule.d.ts
+++ src/models/schedule.d.ts
@@ -10,7 +10,19 @@ export interface Schedule {
 
 export interface ScheduleDay {
   date: string;
-  cadetIds: string[];
+  cadet_ids: string[];
 }
 
 export type ScheduleWithoutId = Omit<Schedule, '_id'>;
+
+/**
+ * The availability of a given user in a given schedule
+ */
+export interface ScheduleAvailability {
+  _id: ObjectID;
+  schedule_id: string;
+  user_id: string;
+  days: string[];
+}
+
+export type ScheduleAvailabilityWithoutId = Omit<ScheduleAvailability, '_id'>;
