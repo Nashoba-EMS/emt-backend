c5dbef5cd39ea6a7450066822cb0577562db62e4
Jeff Taylor-Chang <jeff@jefftc.com>
2021-03-22 11:47:09 -0400

    Removed duplicate camelcase rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,10 @@ module.exports = {
   extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   rules: {
     strict: 2,
-    '@typescript-eslint/camelcase': 1,
+    '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/ban-ts-ignore': 0,
     '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/camelcase': 0
+    '@typescript-eslint/no-var-requires': 0
   }
 };
